1ef7bea40d3555d4fb054f788369756d62bbcadb
quantwothree <dinhquanvfx@gmail.com>
2025-08-19 01:05:00 +0800

    feat: add Player Node class with next, previous links to other nodes

---
+++ app/player_node.py
@@ -0,0 +1,32 @@
+class PlayerNode:
+    def __init__(self, player):
+        self._player = player
+        self._next = None
+        self._previous = None
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, player_node):
+        self._next = player_node
+
+    @property
+    def prev(self):
+        return self._previous
+
+    @prev.setter
+    def prev(self, player_node):
+        self._previous = player_node
+
+    @property
+    def key(self):
+        return self._player.uid
+
+    def __str__(self):
+        return f"Player Node: {str(self._player)}"
