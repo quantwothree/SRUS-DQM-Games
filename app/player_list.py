d0cbe43b23cd04479da38b42ff6b5c77987a39a4
quantwothree <dinhquanvfx@gmail.com>
2025-08-24 09:26:44 +0800

    feat: add tail reference for Player List class
    test: add unit tests for insert at tail functions

--- app/player_list.py
+++ app/player_list.py
@@ -1,6 +1,7 @@
 class PlayerList:
     def __init__(self):
         self._head = None
+        self._tail = None
 
     def is_empty(self):
         if self._head is None:
@@ -11,11 +12,27 @@ class PlayerList:
     def insert_head(self, player_node):
         if self.is_empty():
             self._head = player_node
+            self._tail = player_node
         else:
             player_node.next = self._head
             self._head.previous = player_node
             self._head = player_node
 
+    def insert_tail(self, player_node):
+        if self.is_empty():
+            self._head = player_node
+            self._tail = player_node
+        else:
+            self._tail.next = player_node
+            player_node.previous = self._tail
+            self._tail = player_node
+
     @property
-    def head(self): #for accessing the private head in unit tests
+    def head(self): #for accessing the private head
         return self._head
+
+    @property
+    def tail(self): #for accessing the private tail
+        return self._tail
+
+
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -16,6 +16,7 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(sut.head.player.uid, "01")
         self.assertIsNone(sut.head.previous) #if list was empty then nothing should be on either side of head
         self.assertIsNone(sut.head.next)
+        self.assertEqual(sut.tail.player.uid, "01") #tail should be the same as head
 
     def test_insert_head_not_empty_list(self):
         sut = PlayerList()
@@ -38,6 +39,56 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(sut.head.player.uid, "03")
         self.assertEqual(sut.head.next.player.uid, node.player.uid)
         self.assertIsNone(sut.head.previous)
+        self.assertEqual(sut.tail.player.uid, "02")
+
+    def test_insert_tail_empty_list(self):
+        sut = PlayerList()
+        self.assertTrue(sut.is_empty())  # list should be empty at this point
+
+        player = Player("04", "JimmyBillBob")
+        node = PlayerNode(player)
+        sut.insert_tail(node)
+
+        self.assertFalse(sut.is_empty())
+
+        #head and tail should be the same
+        self.assertEqual(sut.head.player.uid, "04")
+        self.assertEqual(sut.tail.player.uid, "04")
+
+        #nothing should be on either side of head(tail)
+        self.assertIsNone(sut.head.previous)
+        self.assertIsNone(sut.head.next)
+        self.assertIsNone(sut.tail.previous)
+        self.assertIsNone(sut.tail.next)
+
+    def test_insert_tail_not_empty_list(self):
+        sut = PlayerList()
+        self.assertTrue(sut.is_empty())  # list should be empty at this point
+
+        # create 2 players
+        player1= Player("05", "MilesDavis")
+        player2 = Player("06", "KiloMetersDavis")
+
+        # create 2 nodes out of the 2 players
+        node1 = PlayerNode(player1)
+        node2 = PlayerNode(player2)
+
+        # insert the 2 nodes
+        sut.insert_tail(node1)
+        sut.insert_tail(node2)
+
+        #node1 shoudl be head and node2 should be tail
+        self.assertEqual(sut.head.player.uid, "05")
+        self.assertEqual(sut.tail.player.uid, "06")
+
+        #head's next should be tail and tail's previous should be head
+        self.assertEqual(sut.head.next.player.uid, "06")
+        self.assertEqual(sut.tail.previous.player.uid, "05")
+
+        #nothing should be in front of head and nothing should behind tail
+        self.assertIsNone(sut.head.previous)
+        self.assertIsNone(sut.tail.next)
+
 
 if __name__ == "__main__":
     unittest.main()
